1b7464a1032e25c0c803214d84b9b4d82158cc6a
kyounghye <kyoung00828@gmail.com>
2023-07-14 17:17:29 +0900

    #2.3: as와 attrs

--- react-masterclass/src/App.js
+++ react-masterclass/src/App.js
@@ -5,6 +5,17 @@ function App() {
     <Container>
       <Box bgColor="teal" />
       <Circle bgColor="pink" />
+      <Btn>Btn</Btn>
+      <Btn as="a" href="/">
+        a tag
+      </Btn>
+      <div>
+        <Input />
+        <Input />
+        <Input />
+        <Input />
+        <Input />
+      </div>
     </Container>
   );
 }
@@ -19,8 +30,22 @@ const Box = styled.div`
   background-color: ${(props) => props.bgColor};
   width: 100px;
   height: 100px;
+  margin: 5px;
 `;
 
 const Circle = styled(Box)`
   border-radius: 50px;
+`;
+
+const Btn = styled.button`
+  background-color: black;
+  color: white;
+  border-radius: 15px;
+  height: 25px;
+  margin: 5px;
+`;
+
+const Input = styled.input.attrs({ required: true, minLength: "5" })`
+  backgrounf-color: gray;
+  height: 30px;
 `;
