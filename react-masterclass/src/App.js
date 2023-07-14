fac807a359fb58a24a04a5d36a244e518d6a08fb
kyounghye <kyoung00828@gmail.com>
2023-07-14 17:02:38 +0900

    #2.2: props를 사용하여 styled 컴포넌트 확장하기

--- react-masterclass/src/App.js
+++ react-masterclass/src/App.js
@@ -1,5 +1,26 @@
+import styled from "styled-components";
+
 function App() {
-  return;
+  return (
+    <Container>
+      <Box bgColor="teal" />
+      <Circle bgColor="pink" />
+    </Container>
+  );
 }
 
 export default App;
+
+const Container = styled.div`
+  display: flex;
+`;
+
+const Box = styled.div`
+  background-color: ${(props) => props.bgColor};
+  width: 100px;
+  height: 100px;
+`;
+
+const Circle = styled(Box)`
+  border-radius: 50px;
+`;
