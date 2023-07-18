763cc4bbe71b737b2da851e871ad594d47ca1731
kyounghye <kyoung00828@gmail.com>
2023-07-18 11:18:49 +0900

    #2.4:  styled에 애니메이션 적용하기

--- react-masterclass/src/App.js
+++ react-masterclass/src/App.js
@@ -1,21 +1,9 @@
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
 
 function App() {
   return (
     <Container>
-      <Box bgColor="teal" />
-      <Circle bgColor="pink" />
-      <Btn>Btn</Btn>
-      <Btn as="a" href="/">
-        a tag
-      </Btn>
-      <div>
-        <Input />
-        <Input />
-        <Input />
-        <Input />
-        <Input />
-      </div>
+      <Box />
     </Container>
   );
 }
@@ -26,26 +14,18 @@ const Container = styled.div`
   display: flex;
 `;
 
+const animation = keyframes`
+from{
+  transform:rotate(0deg);
+}
+to{
+  transform:rotate(360deg);
+}
+`;
+
 const Box = styled.div`
-  background-color: ${(props) => props.bgColor};
-  width: 100px;
   height: 100px;
-  margin: 5px;
-`;
-
-const Circle = styled(Box)`
-  border-radius: 50px;
-`;
-
-const Btn = styled.button`
-  background-color: black;
-  color: white;
-  border-radius: 15px;
-  height: 25px;
-  margin: 5px;
-`;
-
-const Input = styled.input.attrs({ required: true, minLength: "5" })`
-  backgrounf-color: gray;
-  height: 30px;
+  width: 100px;
+  background-color: pink;
+  animation: ${animation} 1s linear infinite;
 `;
