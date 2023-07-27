23629c86190f52b4df9367bf5ad6288ba3bd1c01
kyounghye <kyoung00828@gmail.com>
2023-07-27 13:59:34 +0900

    #2.7: Themes

--- react-masterclass/src/App.js
+++ react-masterclass/src/App.js
@@ -3,6 +3,7 @@ import styled, { keyframes } from "styled-components";
 function App() {
   return (
     <Container>
+      <h1>Hello</h1>
       <Box>
         <span> :D </span>
       </Box>
@@ -14,14 +15,24 @@ export default App;
 
 const Container = styled.div`
   display: flex;
+  justify-content: center;
+  align-items: center;
+  color: ${(props) => props.theme.textColor};
+  background-color: ${(props) => props.theme.backgroundColor};
 `;
 
 const animation = keyframes`
-from{
+0%{
   transform:rotate(0deg);
+  background-color: pink;
 }
-to{
+50%{
+  transform:rotate(180deg);
+  background-color: tomato;
+}
+100%{
   transform:rotate(360deg);
+  background-color: pink;
 }
 `;
 
@@ -33,6 +44,8 @@ const Box = styled.div`
   align-items: center;
   background-color: pink;
   animation: ${animation} 1s linear infinite;
+  border-radius: 30%;
+  margin: 20px;
   span {
     font-size: 50px;
     &:hover {
